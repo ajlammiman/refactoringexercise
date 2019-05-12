3dfa7a024397c4f8da3bb021fc751d6015dbe930
alammiman <alammiman@Companynet.org>
2019-05-12 18:20:01 +0100

    _

--- MineGame/Board.cs
+++ MineGame/Board.cs
@@ -11,16 +11,42 @@ namespace MineGame
 
     public class Board : IBoard
     {
-        public ISquare[] squares { get; private set; }
+        public ISquare[] Squares { get; private set; }
 
         public Board(IEnumerable<ISquare> squares)
         {
-            this.squares = squares.ToArray();
+            Squares = squares.ToArray();
+            ValidateBoard(Squares);
+        }
+
+        private void ValidateBoard(ISquare[] squares)
+        {
+            var ySequence = squares.Select(s => s.Position.Y).ToArray();
+            var checkSequence = Enumerable.Range(1, ySequence.Max()).ToArray();
+
+            for (int i = 0; i < ySequence.Max(); i++)
+            {
+                if (i > ySequence.Length || ySequence[i] != checkSequence[i])
+                    throw new Exception("Board cannot be created, y axis squares not in sequence");
+
+                ValidateXAxis(squares, ySequence[i]);
+            }
+
+        }
+
+        private static void ValidateXAxis(ISquare[] squares, int y)
+        {
+            var xSequence = squares.Where(s => s.Position.Y == y).Select(s => s.Position.X).ToArray();
+            var checkSequence = Enumerable.Range(1, xSequence.Max()).ToArray();
+
+            for (int i = 0; i < xSequence.Max(); i++)
+                if (i > xSequence.Length || xSequence[i] != checkSequence[i])
+                    throw new Exception("Board cannot be created, x axis squares not in sequence");
         }
 
         public bool IsValidMove(Position position)
         {
-            return squares.Any(s => s.Position.Equals(position));
+            return Squares.Any(s => s.Position.Equals(position));
         }
     }
 }
--- Test/BoardTest.cs
+++ Test/BoardTest.cs
@@ -10,10 +10,10 @@ namespace Test
         [Test]
         public void board_confirms_if_move_is_valid()
         {
-            var squares = new List<Square>() { new Square(new Position(1,1)), new Square(new Position(2,2)) };
+            var squares = new List<Square>() { new Square(new Position(1,1)), new Square(new Position(1,2)) };
             var board = new Board(squares);
 
-            Position position = new Position(2,2);
+            Position position = new Position(1,2);
             var valid = board.IsValidMove(position);
 
             Assert.IsTrue(valid);
@@ -22,13 +22,47 @@ namespace Test
         [Test]
         public void board_confirms_if_move_is_invalid()
         {
-            var squares = new List<Square>() { new Square(new Position(1, 1)), new Square(new Position(2, 2)) };
+            var squares = new List<Square>() { new Square(new Position(1, 1)), new Square(new Position(1, 2)) };
             var board = new Board(squares);
 
             Position position = new Position(3, 3);
             var valid = board.IsValidMove(position);
 
             Assert.IsFalse(valid);
+        }
+
+        [Test]
+        public void squares_on_board_must_follow_numerical_sequence_on_x_axis()
+        {
+            string message = "";
+
+            try
+            {
+                var squares = new List<Square>() { new Square(new Position(1, 1)), new Square(new Position(5, 1)), new Square(new Position(3, 1)) };
+                var board = new Board(squares);
+            }
+            catch(System.Exception e)
+            {
+                message = e.Message;
+            }
+            Assert.AreEqual("Board cannot be created, x axis squares not in sequence", message);
+        }
+
+        [Test]
+        public void squares_on_board_must_follow_numerical_sequence_on_y_axis()
+        {
+            string message = "";
+
+            try
+            {
+                var squares = new List<Square>() { new Square(new Position(1, 1)), new Square(new Position(2, 1)), new Square(new Position(1, 2)), new Square(new Position(2, 3)) };
+                var board = new Board(squares);
+            }
+            catch (System.Exception e)
+            {
+                message = e.Message;
+            }
+            Assert.AreEqual("Board cannot be created, y axis squares not in sequence", message);
         }
     }
 }
