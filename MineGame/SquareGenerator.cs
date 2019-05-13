da29cb9f29108c871497d473962f4302beb57d04
Lammiman <alammiman@Companynet.org>
2019-05-13 13:06:39 +0100

    _

---
+++ MineGame/SquareGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace MineGame
+{
+    public static class SquareGenerator
+    {
+        public static Square[] Generate(int xLength, int yLength)
+        {
+            var squares = new List<Square>();
+
+            for (int y = 0; y < yLength; y++)
+                for (int x = 0; x < xLength; x++)
+                    squares.Add(new Square(new Position(x, y)));
+                
+            return squares.ToArray(); 
+        }
+    }
+}
---
+++ Test/SquareGeneratorTest.cs
@@ -0,0 +1,28 @@
+﻿using MineGame;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Test
+{
+    [TestFixture]
+    class SquareGeneratorTest
+    {
+        [Test]
+        public void squares_created_with_number_of_y_axis_squares_equal_to_seed_value()
+        {
+            var yLength = 8;
+            var squares = SquareGenerator.Generate(1, yLength);
+
+            Assert.That(squares.Select(s => s.Position.Y).Count(), Is.EqualTo(yLength));
+        }
+
+        [Test]
+        public void squares_created_with_number_of_x_axis_squares_equal_to_seed_value()
+        {
+            var xLength = 8;
+            var squares = SquareGenerator.Generate(xLength, 1);
+
+            Assert.That(squares.Select(s => s.Position.X).Count(), Is.EqualTo(xLength));
+        }
+    }
+}
