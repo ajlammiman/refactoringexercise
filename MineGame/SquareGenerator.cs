3d4027bb107ad4201ae5184eff5ead944de6368e
Lammiman <alammiman@Companynet.org>
2019-05-13 13:17:59 +0100

    _

--- MineGame/SquareGenerator.cs
+++ MineGame/SquareGenerator.cs
@@ -8,8 +8,8 @@ namespace MineGame
         {
             var squares = new List<Square>();
 
-            for (int y = 0; y < yLength; y++)
-                for (int x = 0; x < xLength; x++)
+            for (int y = 1; y <= yLength; y++)
+                for (int x = 1; x <= xLength; x++)
                     squares.Add(new Square(new Position(x, y)));
                 
             return squares.ToArray(); 
