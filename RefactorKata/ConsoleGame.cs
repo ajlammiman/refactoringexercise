32aaf0a5696f828c8f138e6a2e148cae76ddb942
alammiman <alammiman@Companynet.org>
2019-06-20 15:59:17 +0100

    _

--- RefactorKata/ConsoleGame.cs
+++ RefactorKata/ConsoleGame.cs
@@ -1,4 +1,5 @@
-﻿using MineGame;
+﻿using System;
+using MineGame;
 
 namespace MineGameConsole
 {
@@ -16,18 +17,39 @@ namespace MineGameConsole
             return "Welcome to the Mine Game, try to cross the board without hitting a Mine. W = Up, X = down, A = left and S = right.";
         }
 
+        public string MoveUp()
+        {
+            var moveState = game.MoveUp();
+            return MoveMessage(moveState, "up");
+        }
+        public string MoveDown()
+        {
+            var moveState = game.MoveDown();
+            return MoveMessage(moveState, "down");
+        }
+        public string MoveLeft()
+        {
+            var moveState = game.MoveLeft();
+            return MoveMessage(moveState, "left");
+        }
         public string MoveRight()
         {
             var moveState = game.MoveRight();
+            return MoveMessage(moveState, "right");
+        }
+
+        private string MoveMessage(MoveState moveState, string direction)
+        {
+            var playerPosition = $"Square {game.PlayerPosition.X},{game.PlayerPosition.Y}";
 
             switch (moveState)
             {
                 case MoveState.Invalid:
                     return "This move is not allowed, you will move off the board.";
                 case MoveState.Mined:
-                    return $"You have moved one square right and hit a mine, your new position is Square 1,2 and your number of lives is {game.LivesLeft}";
+                    return (game.LivesLeft > 0) ? $"You have moved one square {direction} and hit a mine, your new position is {playerPosition} and your number of lives is {game.LivesLeft}" : "You have lost your last life, GAME OVER!";
                 default:
-                    return $"You have moved one square right, your new position is Square {game.PlayerPosition.X},{game.PlayerPosition.Y}";
+                    return $"You have moved one square {direction}, your new position is {playerPosition}";
             }
         }
     }
--- Test/Unit/ConsoleGameTest.cs
+++ Test/Unit/ConsoleGameTest.cs
@@ -9,45 +9,69 @@ namespace Test
     class ConsoleGameTest
     {
         private ConsoleGame consoleGame;
-        private string startMessage;
-        private const string expectedStartMessage = "Welcome to the Mine Game, try to cross the board without hitting a Mine. W = Up, X = down, A = left and S = right.";
-        private const string expectedRightMessage = "You have moved one square right, your new position is Square 1,2";
-        private const string expectedFailureMessage = "This move is not allowed, you will move off the board.";
-        private const string expectedLifeLostMessage = "You have moved one square right and hit a mine, your new position is Square 1,2 and your number of lives is 1";
         private IGame game = Substitute.For<IGame>();
 
         [Test]
         public void when_beginning_game_start_message_is_displayed()
         {
             consoleGame = new ConsoleGame(game);
-            startMessage = consoleGame.Start();
+            var message = consoleGame.Start();
 
-            Assert.AreEqual(startMessage, expectedStartMessage);
+            Assert.AreEqual(message, "Welcome to the Mine Game, try to cross the board without hitting a Mine. W = Up, X = down, A = left and S = right.");
         }
 
         [Test]
-        public void when_making_a_move_right_right_move_message_is_displayed()
+        [TestCase("up")]
+        [TestCase("down")]
+        [TestCase("left")]
+        [TestCase("right")]
+        public void when_making_a_move_valid_move_correct_message_is_displayed(string direction)
         {
             var game = Substitute.For<IGame>();
             game.MoveRight().Returns(MoveState.Valid);
+            game.MoveLeft().Returns(MoveState.Valid);
+            game.MoveUp().Returns(MoveState.Valid);
+            game.MoveDown().Returns(MoveState.Valid);
+
+            game.PlayerPosition.Returns(new Position(1, 2));
+
+            consoleGame = new ConsoleGame(game);
+            string message = "";
+
+            switch (direction)
+            {
+                case "up":
+                    message = consoleGame.MoveUp();
+                    game.Received().MoveUp();
+                    break;
+                case "down":
+                    message = consoleGame.MoveDown();
+                    game.Received().MoveDown();
+                    break;
+                case "left":
+                    message = consoleGame.MoveLeft();
+                    game.Received().MoveLeft();
+                    break;
+                case "right":
+                    message = consoleGame.MoveRight();
+                    game.Received().MoveRight();
+                    break;
+            }
+
+            Assert.AreEqual(message, $"You have moved one square {direction}, your new position is Square 1,2");
+        }
+
+        [Test]
+        public void when_making_an_invalid_move_then_correct_failure_message_is_displayed()
+        {
+            var game = Substitute.For<IGame>();
+            game.MoveRight().Returns(MoveState.Invalid);
             game.PlayerPosition.Returns(new Position(1, 2));
 
             consoleGame = new ConsoleGame(game);
 
-            string rightMessage = consoleGame.MoveRight();
-            Assert.AreEqual(rightMessage, expectedRightMessage);
-        }
-
-        [Test]
-        public void when_making_an_invalid_move_right_then_failure_message_is_displayed()
-        {
-            var game = Substitute.For<IGame>();
-            game.MoveRight().Returns(MoveState.Invalid);
-
-            consoleGame = new ConsoleGame(game);
-
-            string rightMessage = consoleGame.MoveRight();
-            Assert.AreEqual(rightMessage, expectedFailureMessage);
+            string message = consoleGame.MoveRight();
+            Assert.AreEqual(message, "This move is not allowed, you will move off the board.");
         }
 
          [Test]
@@ -56,12 +80,27 @@ namespace Test
             var game = Substitute.For<IGame>();
             game.MoveRight().Returns(MoveState.Mined);
             game.LivesLeft.Returns(1);
+            game.PlayerPosition.Returns(new Position(1, 2));
 
             consoleGame = new ConsoleGame(game);
 
-            string rightMessage = consoleGame.MoveRight();
+            string message = consoleGame.MoveRight();
 
-            Assert.AreEqual(rightMessage, expectedLifeLostMessage);
+            Assert.AreEqual(message, "You have moved one square right and hit a mine, your new position is Square 1,2 and your number of lives is 1");
+        }
+
+        [Test]
+        public void when_last_life_is_lost_game_is_over()
+        {
+            var game = Substitute.For<IGame>();
+            game.MoveRight().Returns(MoveState.Mined);
+            game.LivesLeft.Returns(0);
+            game.PlayerPosition.Returns(new Position(1, 2));
+
+            consoleGame = new ConsoleGame(game);
+            string message = consoleGame.MoveRight();
+
+            Assert.AreEqual(message, "You have lost your last life, GAME OVER!");
         }
     }
 
