12cb6329f85a88a512328ff627ea90184bb3acb0
Lammiman <alammiman@Companynet.org>
2019-09-12 15:12:52 +0100

    Let the awfulising begin!

--- Test/Categorisation/ConsoleGameTest.cs
+++ Test/Categorisation/ConsoleGameTest.cs
@@ -2,6 +2,7 @@
 using MineGameConsole;
 using NSubstitute;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Test
 {
@@ -9,16 +10,16 @@ namespace Test
     class ConsoleGameTest
     {
         private ConsoleGame consoleGame;
-        private IGame game = Substitute.For<IGame>();
-
+        
         [Test]
         public void when_beginning_game_start_message_is_displayed()
         {
-            consoleGame = new ConsoleGame(game);
+            consoleGame = buildGame(1,1, new Position(0,0), 0, 1, false);
             var message = consoleGame.Start();
 
             Assert.AreEqual(message, "Welcome to the Mine Game, try to cross the board without hitting a Mine. W = Up, X = down, A = left and S = right.");
         }
+        
 
         [Test]
         [TestCase("up")]
@@ -27,69 +28,60 @@ namespace Test
         [TestCase("right")]
         public void when_making_a_move_valid_move_correct_message_is_displayed(string direction)
         {
-            var game = Substitute.For<IGame>();
-            game.MoveRight().Returns(MoveState.Valid);
-            game.MoveLeft().Returns(MoveState.Valid);
-            game.MoveUp().Returns(MoveState.Valid);
-            game.MoveDown().Returns(MoveState.Valid);
-
-            game.PlayerPosition.Returns(new Position(1, 2));
-
-            consoleGame = new ConsoleGame(game);
+            consoleGame = buildGame(3, 3, new Position(2,2),0, 1, false);
             string message = "";
-
+            string expectedMessage = "";
+            
             switch (direction)
             {
                 case "up":
                     message = consoleGame.MoveUp();
-                    game.Received().MoveUp();
+                    expectedMessage = $"You have moved one square {direction}, your new position is Square 2,3";
                     break;
                 case "down":
                     message = consoleGame.MoveDown();
-                    game.Received().MoveDown();
+                    expectedMessage = $"You have moved one square {direction}, your new position is Square 2,1";
                     break;
                 case "left":
                     message = consoleGame.MoveLeft();
-                    game.Received().MoveLeft();
+                    expectedMessage = $"You have moved one square {direction}, your new position is Square 1,2";
                     break;
                 case "right":
                     message = consoleGame.MoveRight();
-                    game.Received().MoveRight();
+                    expectedMessage = $"You have moved one square {direction}, your new position is Square 3,2";
                     break;
             }
 
-            Assert.AreEqual(message, $"You have moved one square {direction}, your new position is Square 1,2");
+            Assert.AreEqual(expectedMessage, message);
         }
+
+        
+
 
         [Test]
         public void when_making_an_invalid_move_then_correct_failure_message_is_displayed()
         {
-            var game = GameMock(MoveState.Invalid);
-
-            consoleGame = new ConsoleGame(game);
+            consoleGame = buildGame(1, 1, new Position(1, 1), 0, 1, false);
 
             string message = consoleGame.MoveRight();
             Assert.AreEqual(message, "This move is not allowed, you will move off the board.");
         }
 
          [Test]
-        public void when_making_a_move_to_a_square_with_a_mines_a_life_is_lost()
+        public void when_making_a_move_to_a_square_with_a_mine_a_life_is_lost()
         {
-            var game = GameMock(MoveState.Mined);
-
-            consoleGame = new ConsoleGame(game);
+            consoleGame = buildGame(2, 2, new Position(1, 2), 4, 2, false);
 
             string message = consoleGame.MoveRight();
 
-            Assert.AreEqual(message, "You have moved one square right and hit a mine, your new position is Square 1,2 and your number of lives is 1");
+            Assert.AreEqual("You have moved one square right and hit a mine, your new position is Square 2,2 and your number of lives is 1", message);
         }
 
         [Test]
         public void when_last_life_is_lost_game_is_over()
         {
-            var game = GameMock(MoveState.Mined, 0);
+            consoleGame = buildGame(2, 2, new Position(1, 2), 4, 1, false);
 
-            consoleGame = new ConsoleGame(game);
             string message = consoleGame.MoveRight();
 
             Assert.AreEqual(message, "You have lost your last life, GAME OVER!");
@@ -98,23 +90,30 @@ namespace Test
         [Test]
         public void when_destination_reached_game_is_won()
         {
-            var game = GameMock(MoveState.Valid,1,true);
-
-            consoleGame = new ConsoleGame(game);
+            consoleGame = buildGame(2, 2, new Position(1, 2), 0, 1, true);
             string message = consoleGame.MoveRight();
 
             Assert.AreEqual(message, "Game completed. Congratulations, you've won!");
 
         }
 
-        private IGame GameMock(MoveState moveState, int lives = 1, bool completed = false)
+        private ConsoleGame buildGame(int xLength, int yLength, Position startPosition, int mines, int lives, bool completed)
         {
-            var game = Substitute.For<IGame>();
-            game.MoveRight().Returns(moveState);
-            game.LivesLeft.Returns(lives);
-            game.PlayerPosition.Returns(new Position(1, 2));
-            game.Completed().Returns(completed);
-            return game;
+            var squares = new List<Square>();
+            int mineCount = 0;
+
+            for (int y = 1; y <= yLength; y++)
+                for (int x = 1; x <= xLength; x++)
+                {
+                    var mined = (mineCount <= mines) ? true : false;
+
+                    squares.Add(new Square(new Position(x, y), completed, mined));
+
+                    mineCount++;
+                }
+
+            var game = new Game(new Board(squares), new Player(startPosition, lives));
+            return new ConsoleGame(game);
         }
     }
 
